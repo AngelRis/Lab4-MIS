5dfcaa41a2ebc3c58fc65688b7f4da0370436ef2
AngelRis <105607641+AngelRis@users.noreply.github.com>
2024-12-28 16:31:53 +0100

    adding reminders

--- calendar_app/lib/main.dart
+++ calendar_app/lib/main.dart
@@ -1,14 +1,37 @@
-import 'package:calendar_app/providers/exam_provider.dart';
-import 'package:calendar_app/screens/calendar_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'providers/exam_provider.dart';
+import 'screens/calendar_screen.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  AwesomeNotifications().initialize(
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'basic_channel',
+        channelName: 'Basic notifications',
+        channelDescription: 'Notification channel for basic tests',
+        defaultColor: const Color(0xFF9D50DD),
+        ledColor: Colors.white,
+        importance: NotificationImportance.High,
+      ),
+    ],
+  );
+
+  AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+    if (!isAllowed) {
+      AwesomeNotifications().requestPermissionToSendNotifications();
+    }
+  });
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
--- calendar_app/lib/providers/exam_provider.dart
+++ calendar_app/lib/providers/exam_provider.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../models/exam.dart';
+import '../services/location_service.dart';
+import '../services/notification_service.dart';
 
 class ExamProvider with ChangeNotifier {
   final List<Exam> _exams = [];
+  final LocationService _locationService = LocationService();
+  final NotificationService _notificationService = NotificationService();
+  final Map<String, bool> _notifiedExams = {};
+  ExamProvider() {
+    _notificationService.initialize();
+    _locationService.startListening(_checkProximity);
+  }
 
   List<Exam> get exams => [..._exams];
 
@@ -16,8 +26,10 @@ class ExamProvider with ChangeNotifier {
       locationName: locationName,
     );
     _exams.add(newExam);
+    _notifiedExams[newExam.id] = false;
     notifyListeners();
   }
+
   List<Exam> getExamsForDate(DateTime date) {
     return _exams
         .where((exam) =>
@@ -27,4 +39,21 @@ class ExamProvider with ChangeNotifier {
         .toList();
   }
 
+  void _checkProximity(Position position) async {
+    for (var exam in _exams) {
+      double distance = Geolocator.distanceBetween(
+        position.latitude,
+        position.longitude,
+        exam.location.latitude,
+        exam.location.longitude,
+      );
+      if (distance < 100 && !_notifiedExams[exam.id]!) {
+        await _notificationService.showNotification(
+          "Потсетник за полагање",
+          "Имате испит по ${exam.title} на 100 метри оддалеченост.",
+        );
+        _notifiedExams[exam.id] = true;
+      }
+    }
+  }
 }
